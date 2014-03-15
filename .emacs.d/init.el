a5c8fc5d44c85c291c33ccb3ca15cc13eb89b3a8
Rahul Kumar <rahulsinner@gmail.com>
2014-03-15 14:27:07 +0530

    keychord was making typing unpleasant.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
  '(column-number-mode t)
- '(custom-safe-themes (quote ("410c47e5b36f3beb70b165b52badc13a77dea96ecea4811ec0f53b9d300be9bf" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "7d4d00a2c2a4bba551fcab9bfd9186abe5bfa986080947c2b99ef0b4081cb2a6" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" "90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "1989847d22966b1403bab8c674354b4a2adf6e03e0ffebe097a6bd8a32be1e19" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "4a60f0178f5cfd5eafe73e0fc2699a03da90ddb79ac6dbc73042a591ae216f03" "0f0e3af1ec61d04ff92f238b165dbc6d2a7b4ade7ed9812b4ce6b075e08f49fe" "d7f1c86b425e148be505c689fc157d96323682c947b29ef00cf57b4e4e46e6c7" "9ea054db5cdbd5baa4cda9d373a547435ba88d4e345f4b06f732edbc4f017dc3" "1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" "5339210234ec915d7d3fd87bfeb506bfc436ff7277a55516ab1781ec85c57224" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "9f42bccce1e13fa5017eb8718574db099e85358b9f424db78e7318f86d1be08f" "62b86b142b243071b5adb4d48a0ab89aefd3cf79ee3adc0bb297ea873b36d23f" default)))
+ '(custom-safe-themes (quote ("11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" "410c47e5b36f3beb70b165b52badc13a77dea96ecea4811ec0f53b9d300be9bf" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "7d4d00a2c2a4bba551fcab9bfd9186abe5bfa986080947c2b99ef0b4081cb2a6" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" "90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "1989847d22966b1403bab8c674354b4a2adf6e03e0ffebe097a6bd8a32be1e19" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "4a60f0178f5cfd5eafe73e0fc2699a03da90ddb79ac6dbc73042a591ae216f03" "0f0e3af1ec61d04ff92f238b165dbc6d2a7b4ade7ed9812b4ce6b075e08f49fe" "d7f1c86b425e148be505c689fc157d96323682c947b29ef00cf57b4e4e46e6c7" "9ea054db5cdbd5baa4cda9d373a547435ba88d4e345f4b06f732edbc4f017dc3" "1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" "5339210234ec915d7d3fd87bfeb506bfc436ff7277a55516ab1781ec85c57224" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "9f42bccce1e13fa5017eb8718574db099e85358b9f424db78e7318f86d1be08f" "62b86b142b243071b5adb4d48a0ab89aefd3cf79ee3adc0bb297ea873b36d23f" default)))
  '(global-hl-line-mode t)
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
@@ -39,21 +39,20 @@
 (setq transient-mark-mode t)
 (setq auto-compression-mode t)
 (setq mouse-wheel-mode nil)
-(setq indent-tabs-mode nil)
 (setq make-backup-files t)
 (setq x-select-enable-clipboard t)
 (setq x-select-enable-primary t)
 
+(setq-default  indent-tabs-mode nil)
 (setq-default show-trailing-whitespace t)
 (setq-default global-font-lock-mode t)
 (setq-default fill-column 120)
 (setq-default transient-mark-mode t)
 
+
 (fset 'yes-or-no-p 'y-or-n-p)
 (fset 'perl-mode 'cperl-mode)
 
-(global-linum-mode 1)
-(setq linum-format "%d ")
 
 ;; Automatically load abbreviations.
 (setq-default abbrev-mode t)
@@ -81,6 +80,7 @@
 (global-set-key "\C-x\C-b" 'ibuffer)
 (global-set-key "\C-s" 'isearch-forward-regexp)
 (global-set-key "\C-r" 'isearch-backward-regexp)
+(global-set-key "\M-%" 'query-replace-regexp)
 (global-set-key "\M-/" 'hippie-expand)
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-c\C-w" 'kill-region)
@@ -89,6 +89,7 @@
 
 (load-theme 'zenburn)
 (set-fringe-style '(0 . 2))
+(setq ansi-term-color-vector [unspecified "#3f3f3f" "#cc9393" "#7f9f7f" "#f0dfaf" "#8cd0d3" "#dc8cc3" "#93e0e3" "#dcdccc"])
 
 (autoload 'magit-status "magit" nil t)
 
@@ -103,19 +104,18 @@
 (global-undo-tree-mode 1)
 (global-set-key (kbd "C-x u") 'undo)
 
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-
 (require 'ace-jump-mode)
 (require 'browse-kill-ring)
-(require 'key-chord)
-(key-chord-define-global "jj" 'ace-jump-word-mode)
-(key-chord-define-global "jl" 'ace-jump-line-mode)
-(key-chord-define-global "jk" 'ace-jump-char-mode)
-(key-chord-define-global "uu" 'undo-tree-visualize)
-(key-chord-define-global "xx" 'execute-extended-command)
-(key-chord-define-global "yy" 'browse-kill-ring)
-(key-chord-mode t)
+(global-set-key (kbd "C-c w") 'ace-jump-word-mode)
+(global-set-key (kbd "C-c l") 'ace-jump-line-mode)
+(global-set-key (kbd "C-c c") 'ace-jump-char-mode)
+(global-set-key  (kbd "C-c q") (lambda ()
+                                (interactive)
+                                (insert "\n\n")
+                                (indent-for-tab-command)
+                                (forward-line -1)
+                                (indent-for-tab-command)))
+
 
 (autoload 'web-mode "web-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
@@ -132,6 +132,7 @@
 
 (require 'smartparens-config)
 (smartparens-global-mode 1)
+(show-smartparens-global-mode 1)
 
 (require 'rinari)
 
@@ -156,10 +157,6 @@
 
 (autoload 'org-mode "org" nil t)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
 
 
 (add-hook 'enh-ruby-mode-hook 'inf-ruby-minor-mode)
@@ -177,3 +174,21 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
+
+(require 'scala-mode2)
+(autoload 'ensime-scala-mode-hook "ensime" nil t)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+(add-hook 'term-mode-hook (lambda ()
+			    (setq show-trailing-whitespace nil)
+			    (yas-minor-mode nil)))
+
+(add-hook 'dired-mode-hook (lambda ()
+			     (local-set-key (kbd "j") (lambda ()
+							(interactive)
+							(dired-next-line 1)
+							(image-dired-dired-display-image)))
+			     (local-set-key (kbd "k") (lambda ()
+							(interactive)
+							(dired-previous-line 1)
+							(image-dired-dired-display-image)))))
