7a11517c9d4293cd7995eb4826d388eaffd04b7f
Rahul Kumar <rahulsinner@gmail.com>
2014-07-11 02:21:20 +0530

    Config changes.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,6 +8,7 @@
  '(custom-safe-themes (quote ("a774c5551bc56d7a9c362dca4d73a374582caedb110c201a09b410c0ebbb5e70" "11d069fbfb0510e2b32a5787e26b762898c7e480364cbc0779fe841662e4cf5d" "410c47e5b36f3beb70b165b52badc13a77dea96ecea4811ec0f53b9d300be9bf" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "7d4d00a2c2a4bba551fcab9bfd9186abe5bfa986080947c2b99ef0b4081cb2a6" "9bcb8ee9ea34ec21272bb6a2044016902ad18646bd09fdd65abae1264d258d89" "e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" "90b5269aefee2c5f4029a6a039fb53803725af6f5c96036dee5dc029ff4dff60" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "1989847d22966b1403bab8c674354b4a2adf6e03e0ffebe097a6bd8a32be1e19" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "4a60f0178f5cfd5eafe73e0fc2699a03da90ddb79ac6dbc73042a591ae216f03" "0f0e3af1ec61d04ff92f238b165dbc6d2a7b4ade7ed9812b4ce6b075e08f49fe" "d7f1c86b425e148be505c689fc157d96323682c947b29ef00cf57b4e4e46e6c7" "9ea054db5cdbd5baa4cda9d373a547435ba88d4e345f4b06f732edbc4f017dc3" "1f3304214265481c56341bcee387ef1abb684e4efbccebca0e120be7b1a13589" "5339210234ec915d7d3fd87bfeb506bfc436ff7277a55516ab1781ec85c57224" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "9f42bccce1e13fa5017eb8718574db099e85358b9f424db78e7318f86d1be08f" "62b86b142b243071b5adb4d48a0ab89aefd3cf79ee3adc0bb297ea873b36d23f" default)))
  '(global-hl-line-mode t)
  '(menu-bar-mode nil)
+ '(org-agenda-files (quote ("~/outlines.org")))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(size-indication-mode t)
@@ -48,7 +49,7 @@
 (setq x-select-enable-clipboard t)
 (setq x-select-enable-primary t)
 
-(setq-default  indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)
 (setq-default show-trailing-whitespace t)
 (setq-default global-font-lock-mode t)
 (setq-default fill-column 120)
@@ -89,7 +90,7 @@
 
 
 (global-set-key "\M-g" 'goto-line)
-(global-set-key "\C-xg" 'what-line)
+;(global-set-key "\C-xg" 'what-line)
 (global-set-key "\C-x\C-b" 'ibuffer)
 (global-set-key "\C-s" 'isearch-forward-regexp)
 (global-set-key "\C-r" 'isearch-backward-regexp)
@@ -157,12 +158,14 @@
                       scss-mode
                       smartparens
                       sr-speedbar
+                      tuareg
                       undo-tree
                       web-mode
                       yasnippet
                       zenburn-theme))
 
 (defun install-packages ()
+  (interactive)
   (when (not package-archive-contents)
     (package-refresh-contents))
   (dolist (p my-packages)
@@ -220,12 +223,10 @@
 (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
 
 (define-key sp-keymap (kbd "C-M-d") 'sp-down-sexp)
-(define-key sp-keymap (kbd "C-M-a") 'sp-backward-down-sexp)
 (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
 (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
 
 (define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
-(define-key emacs-lisp-mode-map (kbd ")") 'sp-up-sexp)
 (define-key sp-keymap (kbd "C-M-u") 'sp-backward-up-sexp)
 (define-key sp-keymap (kbd "C-M-t") 'sp-transpose-sexp)
 
@@ -235,7 +236,6 @@
 (define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp)
 (define-key sp-keymap (kbd "C-M-w") 'sp-copy-sexp)
 
-(define-key sp-keymap (kbd "M-<delete>") 'sp-unwrap-sexp)
 (define-key sp-keymap (kbd "M-<backspace>") 'sp-backward-unwrap-sexp)
 
 (define-key sp-keymap (kbd "C-)") 'sp-forward-slurp-sexp)
@@ -244,26 +244,22 @@
 (define-key sp-keymap (kbd "C-{") 'sp-backward-barf-sexp)
 
 (define-key sp-keymap (kbd "M-D") 'sp-splice-sexp)
-(define-key sp-keymap (kbd "C-M-<delete>") 'sp-splice-sexp-killing-forward)
-(define-key sp-keymap (kbd "C-M-<backspace>") 'sp-splice-sexp-killing-backward)
-(define-key sp-keymap (kbd "C-S-<backspace>") 'sp-splice-sexp-killing-around)
 
 (define-key sp-keymap (kbd "C-]") 'sp-select-next-thing-exchange)
-;(define-key sp-keymap (kbd "C-<left_bracket>") 'sp-select-previous-thing)
 (define-key sp-keymap (kbd "C-M-]") 'sp-select-next-thing)
 
 (define-key sp-keymap (kbd "M-F") 'sp-forward-symbol)
 (define-key sp-keymap (kbd "M-B") 'sp-backward-symbol)
 
-(define-key sp-keymap (kbd "H-t") 'sp-prefix-tag-object)
-(define-key sp-keymap (kbd "H-p") 'sp-prefix-pair-object)
-(define-key sp-keymap (kbd "H-s c") 'sp-convolute-sexp)
-(define-key sp-keymap (kbd "H-s a") 'sp-absorb-sexp)
-(define-key sp-keymap (kbd "H-s e") 'sp-emit-sexp)
-(define-key sp-keymap (kbd "H-s p") 'sp-add-to-previous-sexp)
-(define-key sp-keymap (kbd "H-s n") 'sp-add-to-next-sexp)
-(define-key sp-keymap (kbd "H-s j") 'sp-join-sexp)
-(define-key sp-keymap (kbd "H-s s") 'sp-split-sexp)
+;; (define-key sp-keymap (kbd "H-t") 'sp-prefix-tag-object)
+;; (define-key sp-keymap (kbd "H-p") 'sp-prefix-pair-object)
+;; (define-key sp-keymap (kbd "H-s c") 'sp-convolute-sexp)
+;; (define-key sp-keymap (kbd "H-s a") 'sp-absorb-sexp)
+;; (define-key sp-keymap (kbd "H-s e") 'sp-emit-sexp)
+;; (define-key sp-keymap (kbd "H-s p") 'sp-add-to-previous-sexp)
+;; (define-key sp-keymap (kbd "H-s n") 'sp-add-to-next-sexp)
+;; (define-key sp-keymap (kbd "H-s j") 'sp-join-sexp)
+;; (define-key sp-keymap (kbd "H-s s") 'sp-split-sexp)
 
 ;;;;;;;;;;;;;;;;;;
 ;; pair management
@@ -284,10 +280,6 @@
 ;;; html-mode
 (sp-with-modes '(html-mode sgml-mode)
   (sp-local-pair "<" ">"))
-
-;;; lisp modes
-;(sp-with-modes sp--lisp-modes
-;  (sp-local-pair "(" nil :bind "C-("))
 
 (require 'rinari)
 
@@ -426,3 +418,15 @@
 
 (add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
 (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+(autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+(autoload 'tuareg-imenu-set-imenu "tuareg-imenu"
+  "Configuration of imenu for tuareg" t)
+
+(add-hook 'tuareg-mode-hook 'tuareg-imenu-set-imenu)
+
+(setq auto-mode-alist
+      (append '(("\\.ml[ily]?$" . tuareg-mode)
+                ("\\.topml$" . tuareg-mode))
+              auto-mode-alist))
